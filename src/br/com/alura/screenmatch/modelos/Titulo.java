12d873a7bac7078716e7d303bd423752ec423802
Danillo <danillogoncalves001@gmail.com>
2023-05-11 21:30:47 -0300

    Ordenando a lista

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Titulo {
+public class Titulo implements Comparable<Titulo> {
     private String nome;
     private int anoDeLancamento;
     private boolean incluidoNoPLano;
@@ -62,5 +62,10 @@ public class Titulo {
 
     public double pegaMedia() {
         return somaDasAvaliacao / totalDeAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
     }
 }
--- src/br/com/alura/screenmatch/principal/principalComListas.java
+++ src/br/com/alura/screenmatch/principal/principalComListas.java
@@ -5,57 +5,74 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class principalComListas {
     public static void main(String[] args) {
-//        Filme meuFilme = new Filme("O poderoso chefão", 1970);
-//        meuFilme.avalia(9);
-//        Filme outroFilme = new Filme("Avatar", 2023);
-//        outroFilme.avalia(6);
-//        var filmeDoPaulo = new Filme("Dogville", 2003);
-//        filmeDoPaulo.avalia(10);
-//        Serie lost = new Serie("Lost", 2000);
-//
-//        Filme f1 = filmeDoPaulo;
-//
-//        ArrayList<Titulo> lista= new ArrayList<>();
-//        lista.add(filmeDoPaulo);
-//        lista.add(meuFilme);
-//        lista.add(outroFilme);
-//        lista.add(lost);
-//
-//        for (Titulo item : lista) {
-//            System.out.println(item.getNome());
-//            if (item instanceof Filme filme && filme.getClassificacao() > 3) {
-//                System.out.println("Classificação " + filme.getClassificacao());
-//            }
-//        }
-//        filmeDoPaulo.setDiretor("Danillo");
-//        System.out.println(filmeDoPaulo.getDiretor());
-//        System.out.println(f1.getDiretor());
-//        f1.setDiretor("Paty");
-//        System.out.println(filmeDoPaulo.getDiretor());
-//        System.out.println(f1.getDiretor());
-//        lista.forEach(System.out::println);
-        Filme favorito = new Filme("The Matrix", 1999);
-        favorito.avalia(10);
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
 
-        Filme outroFilme = new Filme("John Wick", 2014);
-        outroFilme.avalia(9);
+        Filme f1 = filmeDoPaulo;
 
-        Serie serie = new Serie("La Casa de Papel", 2017);
-
-        ArrayList<Titulo> lista = new ArrayList<>();
-        lista.add(favorito);
+        ArrayList<Titulo> lista= new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
         lista.add(outroFilme);
-        lista.add(serie);
+        lista.add(lost);
 
         for (Titulo item : lista) {
-            System.out.println("Nome: " + item.getNome());
-            if (item instanceof Filme filme) {
-                System.out.println("Classificação: " + filme.getClassificacao());
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 3) {
+                System.out.println("Classificação " + filme.getClassificacao());
             }
         }
+        filmeDoPaulo.setDiretor("Danillo");
+        System.out.println(filmeDoPaulo.getDiretor());
+        System.out.println(f1.getDiretor());
+        f1.setDiretor("Paty");
+        System.out.println(filmeDoPaulo.getDiretor());
+        System.out.println(f1.getDiretor());
+        lista.forEach(System.out::println);
+//        Filme favorito = new Filme("The Matrix", 1999);
+//        favorito.avalia(10);
+//
+//        Filme outroFilme = new Filme("John Wick", 2014);
+//        outroFilme.avalia(9);
+//
+//        Serie serie = new Serie("La Casa de Papel", 2017);
+//
+//        ArrayList<Titulo> lista = new ArrayList<>();
+//        lista.add(favorito);
+//        lista.add(outroFilme);
+//        lista.add(serie);
+//
+//        for (Titulo item : lista) {
+//            System.out.println("Nome: " + item.getNome());
+//            if (item instanceof Filme filme) {
+//                System.out.println("Classificação: " + filme.getClassificacao());
+//            }
+//        }
+
+        ArrayList<String > buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulor");
+        buscaPorArtista.add("Jacqueline");
+
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(lista);
+        System.out.println("Lista de titulos ordenados");
+        System.out.println(lista);
     }
 
 }
