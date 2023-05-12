6467e2a95d93087ffc4e5ab4b8a2da9125f77ca3
Danillo <danillogoncalves001@gmail.com>
2023-05-11 21:01:21 -0300

    Tipos diferentes de objetos na lista Identificando o objeto

---
+++ src/br/com/alura/screenmatch/principal/principalComListas.java
@@ -0,0 +1,42 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class principalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        Filme f1 = filmeDoPaulo;
+
+        ArrayList<Titulo> lista= new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+
+        for (Titulo item : lista) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 3) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+        }
+        filmeDoPaulo.setDiretor("Danillo");
+        System.out.println(filmeDoPaulo.getDiretor());
+        System.out.println(f1.getDiretor());
+        f1.setDiretor("Paty");
+        System.out.println(filmeDoPaulo.getDiretor());
+        System.out.println(f1.getDiretor());
+//        lista.forEach(System.out::println);
+    }
+
+}
